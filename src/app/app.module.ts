90983183edd784537edf169a1e71c044ba46dd5c
kamiazya <yuki@kamiazya.tech>
2019-05-03 02:26:21 +0900

    run schematics:ng-add script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxFaceApiJsModule } from 'ngx-face-api-js';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFaceApiJsModule.forRoot({ modelsUrl: 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights' })
   ],
   providers: [],
   bootstrap: [AppComponent]
