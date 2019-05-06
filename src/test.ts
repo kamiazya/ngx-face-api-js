431b7638a7052d2e59cec4c1be22336b41e1f3bc
kamiazya <yuki@kamiazya.tech>
2019-05-06 22:52:00 +0900

    fix demo app build error

--- src/test.ts
+++ src/test.ts
@@ -4,16 +4,29 @@ import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { NgxFaceApiJsModule } from 'ngx-face-api-js';
 
 declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(),
 );
+
+beforeEach(() => {
+  getTestBed().configureTestingModule({
+    imports: [
+      NgxFaceApiJsModule.forRoot({
+        modelsUrl:
+          'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights',
+      }),
+    ],
+  });
+});
+
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
