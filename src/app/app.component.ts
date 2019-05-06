9d52b324a87b71cc53008346980affa3f01a2541
kamiazya <yuki@kamiazya.tech>
2019-05-06 19:28:43 +0900

    add sample application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngx-face-api-demo-app';
+  imageSrc = '/assets/sample1.jpg';
+
+  selectFile(event: any) {
+    const file = event.target.files.item(0);
+    const reader = new FileReader();
+    reader.onload = (e: any) => (this.imageSrc = e.target.result);
+    reader.readAsDataURL(file);
+  }
 }
